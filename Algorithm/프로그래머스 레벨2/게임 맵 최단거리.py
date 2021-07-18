f064e8fe1bc26e649659be13ab465aac33bb8b2e
JIHYEOK0801 <61791158+JIHYEOK0801@users.noreply.github.com>
2021-07-18 19:32:40 +0900

    Add files via upload

--- Algorithm/프로그래머스 레벨2/게임 맵 최단거리.py
+++ Algorithm/프로그래머스 레벨2/게임 맵 최단거리.py
@@ -1,68 +1,48 @@
+# BFS 활용
+# 한칸 전진시 1인 칸들을 큐에 저장, 저장하면 원래 위치는 0으로 만들고 삭제
 from collections import deque
-def play(start,maps):
-
-    i = start[0]
-    j = start[1]
-    ori = new_map[i][j]
-    new_map[i][j] = 0
-
-    
-
-    if i == end[0] and j == end[1]:
-        switch = 0
-        answer = min(ori, answer)
-        return
-
-    
 
 def solution(maps):
     answer = 0
-    for i in range(len(maps)):
-        maps[i] = [0]+maps[i]+[0]
-    maps = [[0]*len(maps[0])]+maps+[[0]*len(maps[0])]
+
+    m = len(maps)
+    n = len(maps[0])
+
+    ## newmap 만들기
+    newmaps = [[0] * (len(maps[0]) + 2)]
+    for mapp in maps:
+        temp = [0] + mapp + [0]
+        newmaps.append(temp)
+    newmaps.append([0] * (len(maps[0]) + 2))
+
+    if newmaps[1][1] == 0 or newmaps[m][n] == 0:
+        return -1
+    endpoint = [m,n]
+    positions = deque([[1,1]])
+    updownleftright = [[-1,0],[1,0],[0,-1],[0,1]] ## 위, 아래, 왼쪽, 오른쪽
     
-    start = [1,1]
-    end = [len(maps) - 2, len(maps[0]) - 2]
-
-    i = start[0]
-    j = start[1]
-
-    q_for_bfs = deque()
-    
-    while i!= 0 and j!= 0:
-        count = 0
-        if maps[i - 1][j] == 1:
-            q_for_bfs.append([i - 1 , j])
-            count += 1
-
-        if maps[i][j - 1] == 1:
-            q_for_bfs.append([i , j - 1])
-            count += 1
-
-        if maps[i][j + 1] == 1:
-            q_for_bfs.append([i , j + 1])
-            count += 1
-
-        if maps[i + 1][j] == 1:
-            q_for_bfs.append([i + 1 , j])
-            count += 1
-
-        if not q_for_bfs:
+    while(True):
+        if not positions: ## positions에 노드가 없을 때
             return -1
+        if positions[0] == endpoint: ## 해당 노드가 끝 점일때
+            answer = newmaps[positions[0][0]][positions[0][1]]
+            return answer
+        else:  
+            newposi = [] 
+            posi = positions.popleft()
+            
+            for node in updownleftright:
+                if newmaps[posi[0]+node[0]][posi[1]+node[1]] == 1:
+                    newmaps[posi[0]+node[0]][posi[1]+node[1]] += newmaps[posi[0]][posi[1]] # 새로운 노드 value += 현 위치 노드 value
+                    newposi.append([posi[0]+node[0],posi[1]+node[1]])
+            
+            newmaps[posi[0]][posi[1]] = 0 # 원래 있던 위치는 0으로 만들어준다
+            
+            for n in newposi:
+                positions.append(n)
 
-        for k in range(count):
-            nxt = q_for_bfs.popleft()
+            
+            
 
-        i = nxt[0]
-        j = nxt[1]
-        
-        
-        
-    
-    return answer
-    
-    
-    
-
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
+#print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+#print(solution([[1,1,0],[0,1,1]]))
